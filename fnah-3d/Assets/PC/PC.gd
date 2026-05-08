83bc45685efbb83148af84c4e6b7d33f5f4a5f30
Gregorek222 <maciek.gregorczuk@gmail.com>
2026-05-08 21:56:45 +0200

    PC model

---
+++ fnah-3d/Assets/PC/PC.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var view_point = $"../view_point"
+@onready var player = $CharacterBody3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func interact():
+	pass
