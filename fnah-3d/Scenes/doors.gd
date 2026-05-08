df92ec7696e8a5898762b42c85a44290b8dbd656
Gregorek222 <maciek.gregorczuk@gmail.com>
2026-05-09 01:23:02 +0200

    Doors anim

---
+++ fnah-3d/Scenes/doors.gd
@@ -0,0 +1,19 @@
+extends AnimatableBody3D
+
+@onready var animPlayer = $"../../../../AnimationPlayer"
+
+var open:bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func interact():
+	if !open: animPlayer.play("Open Door")
+	else: animPlayer.play("Close Door")
+	open = !open
