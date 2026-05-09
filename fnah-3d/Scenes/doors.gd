2c957ae118f2650d441840cedfe022b45de1af79
mikoolec <148092202+mikoolec@users.noreply.github.com>
2026-05-09 02:18:16 +0200

    FIX THE FUCKING DOOR
    we ball?

--- fnah-3d/Scenes/doors.gd
+++ fnah-3d/Scenes/doors.gd
@@ -6,8 +6,10 @@ var open:bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_as_top_level(false)
 
+func _physics_process(delta: float) -> void:
+	global_transform = get_parent().global_transform
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
