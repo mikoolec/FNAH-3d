afb6a908263b530a918b9611b734e326ed36e5af
Gregorek222 <maciek.gregorczuk@gmail.com>
2026-07-09 16:18:50 +0200

    Email v0.2

--- fnah-3d/Assets/PC/Websites/email_database.gd
+++ fnah-3d/Assets/PC/Websites/email_database.gd
@@ -6,19 +6,19 @@ static var pool = [
 	{
 		"sender": "szef@firma.pl",
 		"subject": "PILNE: Faktura do wydrukowania",
-		"body": "Cześć,\nw załączniku przesyłam zaległą fakturę. Proszę, wydrukuj ją i przekaż do księgowości na już.\n\nPozdrawiam,\nSzef",
+		"body": "\nCześć,\nw załączniku przesyłam zaległą fakturę. Proszę, wydrukuj ją i przekaż do księgowości na już.\n\nPozdrawiam,\nSzef",
 		"attachment": "faktura_05_2026.pdf" # Nazwa pliku załącznika (opcjonalnie)
 	},
 	{
 		"sender": "no-reply@sharepoint-security.pl",
 		"subject": "Twój kod uwierzytelniający MFA",
-		"body": "Wykryto nową próbę logowania do konta SharePoint.\nTwój jednorazowy kod dostępu to: [b]1234[/b].",
+		"body": "\nWykryto nową próbę logowania do konta SharePoint.\nTwój jednorazowy kod dostępu to: [b]1234[/b].",
 		"attachment": "" # Brak załącznika
 	},
 	{
 		"sender": "admin@serwer.local",
 		"subject": "Raport z logowania",
-		"body": "System zarejestrował udane logowanie z nieznanego adresu IP.",
+		"body": "\nSystem zarejestrował udane logowanie z nieznanego adresu IP.",
 		"attachment": "logi_systemowe.txt"
 	}
 ]
