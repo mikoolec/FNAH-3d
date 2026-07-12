59e84671300139120a7e82f42fb74f21083614f4
mikoolec <148092202+mikoolec@users.noreply.github.com>
2026-07-12 12:53:24 +0200

    rename

--- fnah-3d/Scripts/gameplay_numbers.gd
+++ fnah-3d/Scripts/gameplay_numbers.gd
@@ -2,10 +2,10 @@
 extends Node
 
 # Zmienna dostępna z każdego miejsca w projekcie
-var active_zones: int = 0
+var active_ladder_zones: int = 0
 
 func is_player_in_any_zone() -> bool:
-	return active_zones > 0
+	return active_ladder_zones > 0
 
 func reset_zones() -> void:
-	active_zones = 0
+	active_ladder_zones = 0
