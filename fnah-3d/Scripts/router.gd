4e6c3754f01a819c1f001fa4de8b318f332170f5
mikoolec <148092202+mikoolec@users.noreply.github.com>
2026-07-26 03:33:39 +0200

    router aesthetic chanfe :3

--- fnah-3d/Scripts/router.gd
+++ fnah-3d/Scripts/router.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var isWifiWorking = true
 var isRouterOn = false
+var isPressing = false
 @onready var light_mat: StandardMaterial3D = $LIGHT.get_active_material(0).duplicate()
 @onready var anim_player = $AnimationPlayer
 
@@ -13,22 +14,34 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if isPressing:
+		if ( !anim_player.is_playing() ) and isRouterOn:
+			anim_player.play("Up")
+			light_mat.albedo_color = Color(0,1,0)
+			isPressing = false
+		elif ( !anim_player.is_playing() ) and !isRouterOn:
+			anim_player.play("Up")
+			light_mat.albedo_color = Color(1,0,0)
+			isPressing = false
 
 func interact():
 	if anim_player:
 		if ( !anim_player.is_playing() ):
 			if isRouterOn:
 				isRouterOn = false
-				light_mat.albedo_color = Color(1,0,0)
+				isPressing = true
 				anim_player.play("Press")
 			else:
 				isRouterOn = true
 				isWifiWorking = true
-				light_mat.albedo_color = Color(0,1,0)
+				isPressing = true
 				anim_player.play("Press")
 
-func break_wifi():
+func break_school_wifi():
 	isWifiWorking = false
+
+func does_school_wifi_work():
+	if isRouterOn:
+		return isWifiWorking
 	
 	
